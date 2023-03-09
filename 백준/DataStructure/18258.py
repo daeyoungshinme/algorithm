e7af015946c2ac159931449edfb5c713fff590b3
Daeyoung Shin <sdy8603@gmail.com>
2023-03-09 23:27:10 +0900

    docs: 백준 최단경로 11403, 18352 문제 풀이 커밋

--- 백준/DataStructure/18258.py
+++ 백준/DataStructure/18258.py
@@ -17,6 +17,6 @@ for _ in range(n):
     elif commands[0] == 'empty':
         print(0 if queue else 1)
     elif commands[0] == 'front':
-        print(list(queue)[0] if queue else -1)
+        print(queue[0] if queue else -1)
     elif commands[0] == 'back':
-        print(list(queue)[-1] if queue else -1)
+        print(queue[-1] if queue else -1)
