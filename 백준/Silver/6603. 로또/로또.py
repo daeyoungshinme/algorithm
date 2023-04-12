2a31d1d4903d84efa0ee4ab5daaa6d22942b5b16
daeyoungshinme <82262991+daeyoungshinme@users.noreply.github.com>
2023-04-12 12:13:53 +0900

    [Silver II] Title: 로또, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,12 +1,20 @@
-from itertools import combinations
+def dfs(depth, start):
+    global n
+    if depth == 6:
+        print(*result)
+        return
+    
+    for i in range(start, n):
+        result.append(nums[i])
+        dfs(depth +1, i + 1 )
+        result.pop()
 
+result = []
 while True:
     n, *nums = map(int,input().split())
     
     if n == 0:
         break
     
-    for a in list(combinations(nums, 6)):
-        print(*a)
-    
+    dfs(0,0)    
     print()
