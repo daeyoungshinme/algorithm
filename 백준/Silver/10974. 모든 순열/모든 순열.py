7bdcfbd72167a0f20c0f1d0475daaa56ff3a6062
daeyoungshinme <82262991+daeyoungshinme@users.noreply.github.com>
2023-04-12 10:51:02 +0900

    [Silver III] Title: 모든 순열, Time: 148 ms, Memory: 36716 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,22 +1,7 @@
+from itertools import permutations
+
 n = int(input())
 nums = [i for i in range(1,n+1)]
 
-visited = [False] * n
-
-result = []
-
-def dfs(depth):
-    global n
-    if depth == n:
-        print(*result)
-        return
-    
-    for i in range(len(nums)):
-        if not visited[i]:
-            result.append(nums[i])
-            visited[i] = True
-            dfs(depth + 1)
-            result.pop()
-            visited[i] = False
-            
-dfs(0)
+for a in list(permutations(nums, n)):
+    print(*a)
