390a32e460ea9b86d98ebd24677bf455aa80f091
daeyoungshinme <82262991+daeyoungshinme@users.noreply.github.com>
2023-05-04 17:48:54 +0900

    [Gold V] Title: ABCDE, Time: 1512 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,5 +1,8 @@
 
 def set_grapth():
+    """
+    각 지점에서 연결되어 있는 지점을 2차원 배월로 초기화하는 함수
+    """
     global M,N
     for _ in range(M):
         a,b = map(int, input().split())
@@ -8,13 +11,18 @@ def set_grapth():
     
 
 def dfs(n, depth):
+    """
+    각 지점에서 연결되어 있는 지점으로 깊이우선 탐색으로 찾는 함수
+    parameter: n = 현재 지점, depth = 탐색 깊이
+    """
     global result
-    if depth == 5:
-        result = 1
+    if depth == 5:  # 탐색 깊이가 5개 되면 종료
+        result = 1  # 1 출력
         return
     
+    # 각 지점에 연결되어 있는 지점 루프 돌면서 탐색
     for i in graph[n]:
-        if not visited[i] and not result:
+        if not visited[i] and not result:  # 방문하지 않은 지점과 아직 탐색이 종료가 되지 않았으면 처리
             visited[i] = True
             dfs(i, depth + 1)
             visited[i] = False
@@ -26,9 +34,11 @@ visited = [False] * N
 result = 0
 set_grapth()
 
+# 각 지점별로 전부 탐색 처리
 for i in range(N):
-    visited[i] == True
-    dfs(i,0)
-    visited[i] == False
-      
+    if not result:
+        visited[i] == True
+        dfs(i,0)
+        visited[i] == False
+        
 print(result)
